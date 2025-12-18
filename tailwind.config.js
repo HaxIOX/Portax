924a80dcabd27bd4973d1038573587974c1dbc84
HaxIOX <isgeek01@gmail.com>
2025-12-18 17:31:02 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // 如果需要扩展动画，可以在这里添加，
+      // 但刚才的代码是内联样式的动画，所以这里保持默认即可
+      animation: {
+        'gradient': 'gradient 3s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
